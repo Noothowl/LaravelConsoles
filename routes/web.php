bda7a762d3e65a29664853a9548852f372bd3c2a
Noothowl <martinchipoco@gmail.com>
2022-05-23 17:40:11 -0400

    Laravel structures: routes{web+api+view+assets}+navbarview

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,28 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+//Main - root
+// Route::get('/', function () {
+//     return view('home');
+// });
+route::view('/','home')->name("home");
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// route::get('/registrar_juego', function (){
+//     return view('registrar_juego');
+// }); 
+route::view('/registrar_juego','registrar_juego')->name("registrar_juego");
+
+// route::get('/registrar_consola', function (){
+//     return view('registrar_consola');
+// });
+route::view('/registrar_consola','registrar_consola')->name("registrar_consola");
+
+// route::get('/ver_juegos', function (){
+//     return view('ver_juegos');
+// });
+route::view('/ver_juegos','ver_juegos')->name("ver_juegos");
+
+// route::get('/ver_consolas', function (){
+//     return view('ver_consolas');
+// });
+route::view('/ver_consolas','ver_consolas')->name("ver_consolas");
