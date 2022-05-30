098bc60a884fa403a3448d0012894614551feb37
Noothowl <martinchipoco@gmail.com>
2022-05-30 14:09:44 -0400

    Axios config: Include Axios on master; Config Axios js + ver_consolas view

--- resources/views/ver_consolas.blade.php
+++ resources/views/ver_consolas.blade.php
@@ -6,7 +6,7 @@ LaravelConsoles - verConsolas
 @section('content')
     <div class="row mt-5">
         <div class="col-12 col-md-12 col-lg-6 mx-auto">
-            <table class="table table-hover table-board table-striped table-responsive">
+            <table class="table table-hover table-board table-bordered table-striped table-responsive">
                 <thead class="text-light bg-dark bg-gradient">
                     <tr>
                         <td>Nombre</td>
